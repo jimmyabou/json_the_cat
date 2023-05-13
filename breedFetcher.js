0167991cf460d9bcd427ae912b8f9874003e1857
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-12 22:28:08 -0400

    create a new breedftecherTest.js use Mocha and Chai assertion to check the output of out breedftecher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,23 @@
 const request = require('request');
 //const breedName = process.argv[2];
-const fetchBreedDescription = function(breedName,callBack) {
+const fetchBreedDescription = function(breedName, callBack) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-    //console.log("error in the domain :", error);
-      callBack(error,null);
+      //console.log("error in the domain :", error);
+      callBack('error', null);
     } else {
       const data = JSON.parse(body);
       if (data.length === 0) {
         console.log("the breed that you entered does not exist");
+        callBack('error', null);
       } else {
-      // console.log(body);
-      // console.log(typeof body);
-        callBack(null,data);
-      // console.log(data);
-      // console.log(typeof data);
+        // console.log(body);
+        // console.log(typeof body);
+        const desc = data[0]["description"];
+
+        callBack(null, desc);
+        // console.log(data);
+        // console.log(typeof data);
       }
 
     }
